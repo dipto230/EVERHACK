130d30ac0e8b9df15383e20deca457e14e26fd72
dipto230 <diptoroy230305@gmail.com>
2025-11-09 01:56:45 +0530

    nav for mobile also

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import SignUp from './pages/SignUp'
 import Login from './pages/Login'
 export const serverUrl = "http://localhost:8000"
 import { ToastContainer } from 'react-toastify';
 import getCurrentUser from './customHooks/getCurrentUser.js'
+import { useSelector } from 'react-redux'
+import Profile from './pages/Profile.jsx'
 
 const App = () => {
   getCurrentUser()
+  const { userData } = useSelector(state=>state.user);
   return (
     <>
     <ToastContainer />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/login' element={<Login/> }/>
+        <Route path='/signup' element={!userData ? <SignUp /> :<Navigate to={"/"}/>} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/profile' element={userData ?<Profile/> : <Navigate to={"/signup"}/>}/>
         
         </Routes>
     </>
