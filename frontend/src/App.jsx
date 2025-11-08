ca756ff04bc2fc7739ae9c567eb57214e68aa5db
dipto230 <diptoroy230305@gmail.com>
2025-11-08 13:12:52 +0530

    signup section fully done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,13 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import SignUp from './pages/SignUp'
 import Login from './pages/Login'
+export const serverUrl = "http://localhost:8000"
+import { ToastContainer } from 'react-toastify';
 
 const App = () => {
   return (
     <>
+    <ToastContainer />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/signup' element={<SignUp />} />
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -4,13 +4,40 @@ import google from '../assets/google.jpg';
 import {IoEye, IoEyeOutline} from 'react-icons/io5';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { serverUrl } from '../App';
+import { toast } from 'react-toastify';
+import { ClipLoader } from 'react-spinners';
 
 const SignUp = () => {
   const [show, setShow] = useState(false);
   const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("student");
+  const [loading, setLoading] = useState(false);
+  const handleSignup = async () => {
+    setLoading(true);
+    try {
+      const result = await axios.post(serverUrl + "/api/auth/signup", { name, password, email, role }, { withCredentials: true });
+      console.log(result);
+      setLoading(false)
+      navigate("/");
+      toast.done("Signup Successful");
+
+      
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      toast.error(error.response.data.message);
+      
+    }
+    
+  }
   return (
   <div className='bg-[#dddbbd] w-screen h-screen flex items-center justify-center'>
-  <form className='w-[98%] md:w-[800px] h-[600px] bg-white shadow-xl rounded-2xl flex overflow-hidden'>
+  <form className='w-[98%] md:w-[800px] h-[600px] bg-white shadow-xl rounded-2xl flex overflow-hidden' onSubmit={(e)=>e.preventDefault()}>
     {/* Left Side */}
     <div className='md:w-1/2 w-full h-full flex flex-col items-center justify-center gap-3'>
                   <div>
@@ -20,19 +47,19 @@ const SignUp = () => {
                   </div>
                   <div className='flex flex-col gap-1 w-[80%] items-start justify-center px-3'>
                       <label htmlFor='name' className='font-semibold'>Name</label>
-                      <input id='name' type='text' className='border-1 w-[100%] h-[35px] border-[#e7e6e6] text-[15px] px-[20px]' placeholder='Your Name'/>
+                      <input id='name' type='text' className='border-1 w-[100%] h-[35px] border-[#e7e6e6] text-[15px] px-[20px]' placeholder='Your Name' onChange={(e)=>setName(e.target.value)} value={name}/>
                       
                   </div>
                       <div className='flex flex-col gap-1 w-[80%] items-start justify-center px-3'>
                       <label htmlFor='email' className='font-semibold'>Email</label>
-                      <input id='email' type='text' className='border-1 w-[100%] h-[35px] border-[#e7e6e6] text-[15px] px-[20px]' placeholder='Your Email'/>
+                      <input id='email' type='text' className='border-1 w-[100%] h-[35px] border-[#e7e6e6] text-[15px] px-[20px]' placeholder='Your Email' onChange={(e)=>setEmail(e.target.value)} value={email}/>
                       
                   </div>
 
 
                           <div className='flex flex-col gap-1 w-[80%] items-start justify-center px-3 relative'>
                       <label htmlFor='password' className='font-semibold'>Password</label>
-                      <input id='password' type={show ? "text" : "password"} className='border-1 w-[100%] h-[35px] border-[#e7e6e6] text-[15px] px-[20px]' placeholder='Your Password' />
+                      <input id='password' type={show ? "text" : "password"} className='border-1 w-[100%] h-[35px] border-[#e7e6e6] text-[15px] px-[20px]' placeholder='Your Password' onChange={(e)=>setPassword(e.target.value)} value={password}  />
                       { !show ?    <IoEyeOutline className='absolute w-[20px] h-[20px] cursor-pointer right-[5%] bottom-[10%]' onClick={()=>setShow(prev=>!prev)} /> :
                       <IoEye className='absolute w-[20px] h-[20px] cursor-pointer right-[5%] bottom-[10%]' onClick={()=>setShow(prev=>!prev)}  />
                       
@@ -40,11 +67,11 @@ const SignUp = () => {
                     }
                   </div>
                   <div className='flex md:w-[50%] w-[70%] items-center justify-between'>
-    <span className='px-[10px] py-[5px] border-[2px] border-[#e7e6e6] rounded-2xl cursor-pointer hover:border-black'>Student</span>
-    <span className='px-[10px] py-[5px] border-[2px] border-[#e7e6e6] rounded-2xl cursor-pointer hover:border-black'>Educator</span>
+    <span className={`px-[10px] py-[5px] border-[2px] border-[#e7e6e6] rounded-2xl cursor-pointer hover:border-black ${role === "student" ? "border-black" :"border-[#646464]"}`} onClick={()=>setRole("student")}>Student</span>
+    <span className={`px-[10px] py-[5px] border-[2px] border-[#e7e6e6] rounded-2xl cursor-pointer hover:border-black ${role === "educator" ? "border-black" :"border-[#646464]"}`} onClick={()=>setRole("educator")}>Educator</span>
                   </div>
-                  <button className='w-[80%] h-[40px] bg-black text-white cursor-pointer  flex items-center justify-center rounded-[5px]'>
-                      SignUp
+                  <button className='w-[80%] h-[40px] bg-black text-white cursor-pointer  flex items-center justify-center rounded-[5px]' onClick={handleSignup} disabled={loading}>
+                      {loading ? <ClipLoader  size={30} color='white'/> :"SignUp"}
                   </button>
                   <div className='w-[80%] flex items-center gap-2'>
                       <div className='w-[25%] h-[0.5px] bg-[#c4c4c4] '></div>
